7b603e98c80c1530d5ad4d19d5ea7901e316bb29
Diego <diegobarbozacoto@gmail.com>
2023-02-08 23:01:21 -0600

    menu lateral

--- ControlCasos/ControlCasos/Form1.cs
+++ ControlCasos/ControlCasos/Form1.cs
@@ -10,11 +10,105 @@ using System.Windows.Forms;
 
 namespace ControlCasos
 {
-    public partial class Form1 : Form
+    public partial class frmPrincipal : Form
     {
-        public Form1()
+        public frmPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void ocultarSubMenu()
+        {
+            if (pnlSubMenuProductos.Visible == true)
+                pnlSubMenuProductos.Visible = false;
+            if (pnlSubMenuClientes.Visible == true)
+                pnlSubMenuClientes.Visible = false;
+            if (pnlSubMenuUsuarios.Visible == true)
+                pnlSubMenuUsuarios.Visible = false;
+        }
+
+        private void mostrarSubMenu(Panel subMenu)
+        {
+            if(subMenu.Visible == false)
+            {
+                ocultarSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+            {
+                subMenu.Visible = false;
+            }
+        }
+
+#region Productos
+        private void Productos_Click(object sender, EventArgs e)
+        {
+            mostrarSubMenu(pnlSubMenuProductos);
+        }
+
+        private void btnTipoProducto_Click(object sender, EventArgs e)
+        {
+            ocultarSubMenu();
+        }
+
+        private void btnColores_Click(object sender, EventArgs e)
+        {
+            ocultarSubMenu();
+        }
+
+        private void btnMarcas_Click(object sender, EventArgs e)
+        {
+            ocultarSubMenu();
+        }
+
+        #endregion
+
+#region Clientes
+        private void btnClientesMenu_Click(object sender, EventArgs e)
+        {
+            mostrarSubMenu(pnlSubMenuClientes);
+        }
+
+        private void btnClientes_Click(object sender, EventArgs e)
+        {
+            ocultarSubMenu();
+        }
+
+        private void btnDoctores_Click(object sender, EventArgs e)
+        {
+            ocultarSubMenu();
+        }
+
+        private void btnPacientes_Click(object sender, EventArgs e)
+        {
+            ocultarSubMenu();
+        }
+
+        #endregion
+
+        private void btnUsuariosMenu_Click(object sender, EventArgs e)
+        {
+            mostrarSubMenu(pnlSubMenuUsuarios);
+        }
+
+        private void btnUsuarios_Click(object sender, EventArgs e)
+        {
+            ocultarSubMenu();
+        }
+
+        private void btnCambioContrasena_Click(object sender, EventArgs e)
+        {
+            ocultarSubMenu();
+        }
+
+        private void btnRespaldos_Click(object sender, EventArgs e)
+        {
+            ocultarSubMenu();
+        }
+
+        private void btnAyuda_Click(object sender, EventArgs e)
+        {
+            ocultarSubMenu();
         }
     }
 }
