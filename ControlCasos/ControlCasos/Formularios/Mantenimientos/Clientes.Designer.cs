a908850bc4b7fe49f9b71b340d1d7a166818ea1b
Diego <diegobarbozacoto@gmail.com>
2023-02-17 14:47:28 -0600

    filtro funcionando

--- ControlCasos/ControlCasos/Formularios/Mantenimientos/Clientes.Designer.cs
+++ ControlCasos/ControlCasos/Formularios/Mantenimientos/Clientes.Designer.cs
@@ -31,6 +31,9 @@
             this.dgvListaClientes = new System.Windows.Forms.DataGridView();
             this.Cliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvListaClientes)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,11 +65,39 @@
             this.Estado.MinimumWidth = 6;
             this.Estado.Name = "Estado";
             // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(87, 47);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(264, 26);
+            this.txtBuscar.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(413, 49);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(33, 33);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(374, 49);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(33, 33);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
             // frmClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(860, 666);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
             this.Controls.Add(this.dgvListaClientes);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -75,6 +106,7 @@
             this.Text = "Clientes";
             ((System.ComponentModel.ISupportInitialize)(this.dgvListaClientes)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,5 +115,8 @@
         private System.Windows.Forms.DataGridView dgvListaClientes;
         private System.Windows.Forms.DataGridViewTextBoxColumn Cliente;
         private System.Windows.Forms.DataGridViewTextBoxColumn Estado;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnCancelar;
     }
 }
--- ControlCasos/ControlCasos/Formularios/Mantenimientos/Clientes.cs
+++ ControlCasos/ControlCasos/Formularios/Mantenimientos/Clientes.cs
@@ -14,18 +14,31 @@ namespace ControlCasos.Formularios.Mantenimientos
 {
     public partial class frmClientes : Form
     {
+        private BLCliente clientes = new BLCliente();
         public frmClientes()
         {
             InitializeComponent();
             cargarDatosEnGrid();
         }
 
-        void cargarDatosEnGrid()
+        private void cargarDatosEnGrid()
         {
-            BLCliente clientes = new BLCliente();
             IList<sp_Cliente_Consultar_Result> fuenteDatos = clientes.consultarClientes(null);//null es para que consulte todos
             dgvListaClientes.AutoGenerateColumns = false;
             dgvListaClientes.DataSource= fuenteDatos;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            IList<sp_Cliente_Consultar_Result> fuenteDatos = clientes.consultarClientes(txtBuscar.Text);
+            dgvListaClientes.AutoGenerateColumns = false;
+            dgvListaClientes.DataSource = fuenteDatos;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtBuscar.Text = "";
+            cargarDatosEnGrid();
         }
     }
 }
