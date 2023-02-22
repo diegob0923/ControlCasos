31023fc43df3695616f5ab0e6df621c92bb51e08
Diego <diegobarbozacoto@gmail.com>
2023-02-21 23:54:37 -0600

    marca mostrando lista

--- ControlCasos/ControlCasos/Formularios/Mantenimientos/Marcas.Designer.cs
+++ ControlCasos/ControlCasos/Formularios/Mantenimientos/Marcas.Designer.cs
@@ -28,22 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvListaMarcas = new System.Windows.Forms.DataGridView();
+            this.Marca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaMarcas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvListaMarcas
+            // 
+            this.dgvListaMarcas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListaMarcas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Marca});
+            this.dgvListaMarcas.Location = new System.Drawing.Point(88, 150);
+            this.dgvListaMarcas.Name = "dgvListaMarcas";
+            this.dgvListaMarcas.RowHeadersWidth = 51;
+            this.dgvListaMarcas.RowTemplate.Height = 24;
+            this.dgvListaMarcas.Size = new System.Drawing.Size(666, 150);
+            this.dgvListaMarcas.TabIndex = 0;
+            // 
+            // Marca
+            // 
+            this.Marca.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Marca.DataPropertyName = "Marca";
+            this.Marca.HeaderText = "Marca";
+            this.Marca.MinimumWidth = 6;
+            this.Marca.Name = "Marca";
             // 
             // frmMarcas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(860, 666);
+            this.Controls.Add(this.dgvListaMarcas);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmMarcas";
             this.Text = "Marcas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaMarcas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvListaMarcas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Marca;
     }
 }
--- ControlCasos/ControlCasos/Formularios/Mantenimientos/Marcas.cs
+++ ControlCasos/ControlCasos/Formularios/Mantenimientos/Marcas.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ControlCasos.BL;
+using ControlCasos.Modelos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,18 @@ namespace ControlCasos.Formularios.Mantenimientos
 {
     public partial class frmMarcas : Form
     {
+        private BLMarca marcas = new BLMarca();
         public frmMarcas()
         {
             InitializeComponent();
+            cargarDatosEnGrid();
+        }
+
+        public void cargarDatosEnGrid()
+        {
+            IList<sp_Marca_Consultar_Result> fuenteDatos = marcas.consultarMarcas(null);//null es para que consulte todos
+            dgvListaMarcas.AutoGenerateColumns = false;
+            dgvListaMarcas.DataSource = fuenteDatos;
         }
     }
 }
