8544290cdb8c22107016f6439cf760feabfb183f
Diego <diegobarbozacoto@gmail.com>
2023-02-24 23:37:51 -0600

    doctores lista, filtro y se actualiza modelo

---
+++ ControlCasos/ControlCasos/BL/BLDoctor.cs
@@ -0,0 +1,30 @@
+﻿using ControlCasos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlCasos.BL
+{
+    class BLDoctor
+    {
+        private ControlCasosEntities modeloBD = new ControlCasosEntities();
+
+        public IList<sp_Doctor_Consultar_Result> consultarDoctores(string nombreDoctor = null)
+        {
+            IList<sp_Doctor_Consultar_Result> listaDoctores = modeloBD.sp_Doctor_Consultar(nombreDoctor).ToList();
+            return listaDoctores;
+        }
+
+        /*public bool insertarCliente(string doctor)
+        {
+            int registrosAfectados = modeloBD.sp_Cliente_Insertar(cliente);
+
+            if (registrosAfectados > 0)
+                return true;
+
+            return false;
+        }*/
+    }
+}
--- ControlCasos/ControlCasos/Formularios/Mantenimientos/Doctores.cs
+++ ControlCasos/ControlCasos/Formularios/Mantenimientos/Doctores.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ControlCasos.BL;
+using ControlCasos.Modelos;
 
 namespace ControlCasos.Formularios.Mantenimientos
 {
     public partial class frmDoctores : Form
     {
+        private BLDoctor doctores = new BLDoctor();
         public frmDoctores()
         {
             InitializeComponent();
+            cargarDatosEnGrid();
+        }
+
+        public void cargarDatosEnGrid()
+        {
+            IList<sp_Doctor_Consultar_Result> fuenteDatos = doctores.consultarDoctores(null);//null es para que consulte todos
+            dgvListaDoctores.AutoGenerateColumns = false;
+            dgvListaDoctores.DataSource = fuenteDatos;
+        }
+
+        #region Filtro
+
+        #endregion
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            if (txtBuscar.Text != "")
+                btnCancelar.Visible = true;
+            else
+                btnCancelar.Visible = false;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtBuscar.Text = "";
+            cargarDatosEnGrid();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            IList<sp_Doctor_Consultar_Result> fuenteDatos = doctores.consultarDoctores(txtBuscar.Text);
+            dgvListaDoctores.AutoGenerateColumns = false;
+            dgvListaDoctores.DataSource = fuenteDatos;
         }
     }
 }
