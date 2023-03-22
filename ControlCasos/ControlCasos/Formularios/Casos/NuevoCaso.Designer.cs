79fbc1d1844dec890d6146f21b57bd69a4cec4b9
Diego <diegobarbozacoto@gmail.com>
2023-03-22 15:42:23 -0600

    cambio distribución de campos para info producto

--- ControlCasos/ControlCasos/Formularios/Casos/NuevoCaso.Designer.cs
+++ ControlCasos/ControlCasos/Formularios/Casos/NuevoCaso.Designer.cs
@@ -102,7 +102,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 78);
+            this.label4.Location = new System.Drawing.Point(370, 81);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(69, 20);
@@ -112,7 +112,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(332, 160);
+            this.label6.Location = new System.Drawing.Point(344, 160);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(95, 20);
@@ -122,7 +122,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(314, 117);
+            this.label7.Location = new System.Drawing.Point(4, 81);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(113, 20);
@@ -132,7 +132,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(371, 75);
+            this.label8.Location = new System.Drawing.Point(61, 132);
             this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(56, 20);
@@ -142,7 +142,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(37, 207);
+            this.label9.Location = new System.Drawing.Point(68, 176);
             this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(49, 20);
@@ -152,7 +152,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(11, 163);
+            this.label10.Location = new System.Drawing.Point(42, 220);
             this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(75, 20);
@@ -162,7 +162,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(8, 121);
+            this.label11.Location = new System.Drawing.Point(361, 124);
             this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(78, 20);
@@ -171,29 +171,29 @@
             // 
             // txtComentario
             // 
-            this.txtComentario.Location = new System.Drawing.Point(446, 160);
+            this.txtComentario.Location = new System.Drawing.Point(455, 161);
             this.txtComentario.Multiline = true;
             this.txtComentario.Name = "txtComentario";
-            this.txtComentario.Size = new System.Drawing.Size(196, 85);
+            this.txtComentario.Size = new System.Drawing.Size(186, 85);
             this.txtComentario.TabIndex = 11;
             // 
             // txtTamano
             // 
-            this.txtTamano.Location = new System.Drawing.Point(102, 75);
+            this.txtTamano.Location = new System.Drawing.Point(455, 78);
             this.txtTamano.Name = "txtTamano";
             this.txtTamano.Size = new System.Drawing.Size(186, 26);
             this.txtTamano.TabIndex = 12;
             // 
             // txtDiametro
             // 
-            this.txtDiametro.Location = new System.Drawing.Point(102, 120);
+            this.txtDiametro.Location = new System.Drawing.Point(455, 123);
             this.txtDiametro.Name = "txtDiametro";
             this.txtDiametro.Size = new System.Drawing.Size(186, 26);
             this.txtDiametro.TabIndex = 13;
             // 
             // txtCantidad
             // 
-            this.txtCantidad.Location = new System.Drawing.Point(102, 163);
+            this.txtCantidad.Location = new System.Drawing.Point(133, 220);
             this.txtCantidad.Name = "txtCantidad";
             this.txtCantidad.Size = new System.Drawing.Size(186, 26);
             this.txtCantidad.TabIndex = 14;
@@ -202,7 +202,7 @@
             // 
             this.cmbColor.DisplayMember = "ColorGuia";
             this.cmbColor.FormattingEnabled = true;
-            this.cmbColor.Location = new System.Drawing.Point(102, 207);
+            this.cmbColor.Location = new System.Drawing.Point(133, 176);
             this.cmbColor.Name = "cmbColor";
             this.cmbColor.Size = new System.Drawing.Size(186, 28);
             this.cmbColor.TabIndex = 15;
@@ -213,9 +213,9 @@
             // 
             this.cmbMarca.DisplayMember = "Marca";
             this.cmbMarca.FormattingEnabled = true;
-            this.cmbMarca.Location = new System.Drawing.Point(446, 75);
+            this.cmbMarca.Location = new System.Drawing.Point(136, 132);
             this.cmbMarca.Name = "cmbMarca";
-            this.cmbMarca.Size = new System.Drawing.Size(196, 28);
+            this.cmbMarca.Size = new System.Drawing.Size(183, 28);
             this.cmbMarca.TabIndex = 16;
             this.cmbMarca.Text = " ";
             this.cmbMarca.ValueMember = "idMarca";
@@ -224,9 +224,9 @@
             // 
             this.cmbTipoProducto.DisplayMember = "TipoProducto";
             this.cmbTipoProducto.FormattingEnabled = true;
-            this.cmbTipoProducto.Location = new System.Drawing.Point(446, 118);
+            this.cmbTipoProducto.Location = new System.Drawing.Point(136, 82);
             this.cmbTipoProducto.Name = "cmbTipoProducto";
-            this.cmbTipoProducto.Size = new System.Drawing.Size(196, 28);
+            this.cmbTipoProducto.Size = new System.Drawing.Size(183, 28);
             this.cmbTipoProducto.TabIndex = 17;
             this.cmbTipoProducto.Text = " ";
             this.cmbTipoProducto.ValueMember = "IdTipoProducto";
@@ -371,19 +371,19 @@
             this.pnlProducto.Controls.Add(this.btnAgregar);
             this.pnlProducto.Controls.Add(this.label12);
             this.pnlProducto.Controls.Add(this.label4);
+            this.pnlProducto.Controls.Add(this.cmbTipoProducto);
             this.pnlProducto.Controls.Add(this.label9);
             this.pnlProducto.Controls.Add(this.label10);
-            this.pnlProducto.Controls.Add(this.cmbTipoProducto);
             this.pnlProducto.Controls.Add(this.label11);
             this.pnlProducto.Controls.Add(this.cmbMarca);
-            this.pnlProducto.Controls.Add(this.txtTamano);
-            this.pnlProducto.Controls.Add(this.txtComentario);
-            this.pnlProducto.Controls.Add(this.cmbColor);
-            this.pnlProducto.Controls.Add(this.label8);
-            this.pnlProducto.Controls.Add(this.label7);
             this.pnlProducto.Controls.Add(this.txtDiametro);
-            this.pnlProducto.Controls.Add(this.label6);
+            this.pnlProducto.Controls.Add(this.txtComentario);
+            this.pnlProducto.Controls.Add(this.txtTamano);
+            this.pnlProducto.Controls.Add(this.label8);
             this.pnlProducto.Controls.Add(this.txtCantidad);
+            this.pnlProducto.Controls.Add(this.label7);
+            this.pnlProducto.Controls.Add(this.label6);
+            this.pnlProducto.Controls.Add(this.cmbColor);
             this.pnlProducto.Location = new System.Drawing.Point(417, 12);
             this.pnlProducto.Name = "pnlProducto";
             this.pnlProducto.Size = new System.Drawing.Size(662, 302);
