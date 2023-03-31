121a71e61dda46b53f446ba00a63e729c7647073
Diego <diegobarbozacoto@gmail.com>
2023-03-31 15:19:44 -0600

    Validacion en contol de casos

--- ControlCasos/ControlCasos/Formularios/Casos/NuevoCaso.cs
+++ ControlCasos/ControlCasos/Formularios/Casos/NuevoCaso.cs
@@ -1,4 +1,4 @@
-﻿using ControlCasos.BL;
+using ControlCasos.BL;
 using ControlCasos.Modelos;
 using ControlCasos.Clases;
 using System;
@@ -104,21 +104,40 @@ namespace ControlCasos.Formularios.Casos
         #endregion
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-            Producto nuevoProducto = new Producto();
-            nuevoProducto.tamano = txtTamano.Text;
-            nuevoProducto.diametro = txtDiametro.Text;
-            nuevoProducto.cantidad = byte.Parse(txtCantidad.Text);
-            nuevoProducto.idColor = byte.Parse(cmbColor.SelectedValue.ToString());
-            nuevoProducto.color = cmbColor.Text;
-            nuevoProducto.idMarca = byte.Parse(cmbMarca.SelectedValue.ToString());
-            nuevoProducto.marca = cmbMarca.Text;
-            nuevoProducto.idTipoProducto = byte.Parse(cmbTipoProducto.SelectedValue.ToString());
-            nuevoProducto.tipoProducto = cmbTipoProducto.Text;
-            nuevoProducto.comentario = txtComentario.Text;
+            //controles que quiero validar
+            cmbTipoProducto.CausesValidation = true;
+            cmbMarca.CausesValidation = true;
+            cmbColor.CausesValidation = true;
 
-            listaProductos.Add(nuevoProducto);
-            cargarDatosEnGrid();
-            limpiarDatosProducto();
+            //controles que no quiero validar
+            cmbDoctor.CausesValidation = false;
+            txtPaciente.CausesValidation = false;
+
+            if (ValidateChildren(ValidationConstraints.Enabled))
+            {
+                try
+                {
+                    Producto nuevoProducto = new Producto();
+                    nuevoProducto.tamano = txtTamano.Text;
+                    nuevoProducto.diametro = txtDiametro.Text;
+                    nuevoProducto.cantidad = byte.Parse(nudCantidad.Text);
+                    nuevoProducto.idColor = byte.Parse(cmbColor.SelectedValue.ToString());
+                    nuevoProducto.color = cmbColor.Text;
+                    nuevoProducto.idMarca = byte.Parse(cmbMarca.SelectedValue.ToString());
+                    nuevoProducto.marca = cmbMarca.Text;
+                    nuevoProducto.idTipoProducto = byte.Parse(cmbTipoProducto.SelectedValue.ToString());
+                    nuevoProducto.tipoProducto = cmbTipoProducto.Text;
+                    nuevoProducto.comentario = txtComentario.Text;
+
+                    listaProductos.Add(nuevoProducto);
+                    cargarDatosEnGrid();
+                    limpiarDatosProducto();
+                }
+                catch (Exception) 
+                {
+                    MessageBox.Show("Ocurrio un error al agregar el producto");
+                }
+            } 
         }
 
         public void cargarDatosEnGrid()
@@ -140,7 +159,7 @@ namespace ControlCasos.Formularios.Casos
         {
             txtTamano.Text = "";
             txtDiametro.Text = "";
-            txtCantidad.Text = "";
+            nudCantidad.Text = "";
             cmbColor.SelectedIndex = 0;
             cmbMarca.SelectedIndex = 0;
             cmbTipoProducto.SelectedIndex = 0;
@@ -180,75 +199,87 @@ namespace ControlCasos.Formularios.Casos
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            if (listaProductos.Count != 0 && cmbDoctor.SelectedIndex != 0 && txtPaciente.Text != "")
+            //controles que quiero validar
+            cmbDoctor.CausesValidation = true;
+            txtPaciente.CausesValidation = true;
+
+            //controles que no quiero validar
+            cmbTipoProducto.CausesValidation = false;
+            cmbMarca.CausesValidation = false;
+            cmbColor.CausesValidation = false;
+
+            if (ValidateChildren(ValidationConstraints.Enabled))
             {
-                try
+                if (listaProductos.Count != 0 && cmbDoctor.SelectedIndex != 0 && txtPaciente.Text != "")
                 {
-                    IList<int> listaRegistrosInsertados = new List<int>();
-                    int idNuevoRegistro = 0;
-                    int maxCasoId = 0;
-
-                    if(BLCasos.insertarCaso(dtpFecha.Value, int.Parse(cmbDoctor.SelectedValue.ToString()),
-                                            darFormatoDeTituloAlStringPaciente(txtPaciente.Text)))
+                    try
                     {
+                        IList<int> listaRegistrosInsertados = new List<int>();
+                        int idNuevoRegistro = 0;
+                        int maxCasoId = 0;
 
-                        maxCasoId = BLCasos.consultarCasoMaxID();//aquí el caso ya se creo exitosamente, recuperamos el id de ese caso recien insertado para asignar los productos con este caso
-
-                        try
+                        if (BLCasos.insertarCaso(dtpFecha.Value, int.Parse(cmbDoctor.SelectedValue.ToString()),
+                                                darFormatoDeTituloAlStringPaciente(txtPaciente.Text)))
                         {
-                            foreach (Producto producto in listaProductos)
-                            {
-                                idNuevoRegistro = BLCasos.insertarProducto(producto.idColor, producto.idMarca, producto.idTipoProducto, maxCasoId, producto.tamano, producto.diametro, producto.cantidad, producto.comentario);
 
-                                if (idNuevoRegistro != ErrorAlInsertarProducto)
+                            maxCasoId = BLCasos.consultarCasoMaxID();//aquí el caso ya se creo exitosamente, recuperamos el id de ese caso recien insertado para asignar los productos con este caso
+
+                            try
+                            {
+                                foreach (Producto producto in listaProductos)
                                 {
-                                    listaRegistrosInsertados.Add(idNuevoRegistro);
+                                    idNuevoRegistro = BLCasos.insertarProducto(producto.idColor, producto.idMarca, producto.idTipoProducto, maxCasoId, producto.tamano, producto.diametro, producto.cantidad, producto.comentario);
+
+                                    if (idNuevoRegistro != ErrorAlInsertarProducto)
+                                    {
+                                        listaRegistrosInsertados.Add(idNuevoRegistro);
+                                    }
+                                    else
+                                    {
+                                        throw new Exception();
+                                    }
                                 }
-                                else
+                            }
+                            catch (Exception)
+                            {
+                                //eliminar posibles productos que se hayan creado
+                                if (listaRegistrosInsertados.Count != 0)//si la lista tiene registros se empiezan a eliminar
                                 {
-                                    throw new Exception();
+                                    foreach (int idProducto in listaRegistrosInsertados)
+                                    {
+                                        BLCasos.eliminarProducto(idProducto);
+                                    }
                                 }
+
+                                //eliminar ultimo caso creado
+                                if (maxCasoId != 0)
+                                {
+                                    BLCasos.eliminarCaso(maxCasoId);
+                                }
+
+                                limpiarTodoSiError();
+                                throw new Exception();
                             }
                         }
-                        catch (Exception)
-                        {
-                            //eliminar posibles productos que se hayan creado
-                            if (listaRegistrosInsertados.Count != 0)//si la lista tiene registros se empiezan a eliminar
-                            {
-                                foreach (int idProducto in listaRegistrosInsertados)
-                                {
-                                    BLCasos.eliminarProducto(idProducto);
-                                }
-                            }
 
-                            //eliminar ultimo caso creado
-                            if (maxCasoId != 0)
-                            {
-                                BLCasos.eliminarCaso(maxCasoId);
-                            }
+                        MessageBox.Show("Nuevo caso creado satisfactoriamente");
+                        this.Dispose();
+                        formularioCasos.recargarComboPacienteLuegoDeInsertarNuevoCaso();//esto va a actualizar la lista de casos para que los nuevos registros sean visibles
 
-                            limpiarTodoSiError();
-                            throw new Exception();
-                        }
                     }
-
-                    MessageBox.Show("Nuevo caso creado satisfactoriamente");
-                    this.Dispose();
-                    formularioCasos.recargarComboPacienteLuegoDeInsertarNuevoCaso();//esto va a actualizar la lista de casos para que los nuevos registros sean visibles
-
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Ocurrio un error al crear el caso");
+                    }
                 }
-                catch (Exception)
+                else
                 {
-                    MessageBox.Show("Ocurrio un error al crear el caso");
+                    if (listaProductos.Count == 0)
+                        MessageBox.Show("Por favor agregue al menos un producto");
+                    if (cmbDoctor.SelectedIndex == 0 || txtPaciente.Text == "")
+                        MessageBox.Show("Por favor ingrese un doctor y un paciente ");
                 }
             }
-            else
-            {
-                if (listaProductos.Count == 0)
-                    MessageBox.Show("Por favor agregue al menos un producto");
-                if (cmbDoctor.SelectedIndex == 0 || txtPaciente.Text == "")
-                    MessageBox.Show("Por favor ingrese un doctor y un paciente ");
-            } 
         }
         /// <summary>
         /// cambia el formato del nombre para que sea tipo título
@@ -260,6 +291,113 @@ namespace ControlCasos.Formularios.Casos
         {
             TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
             return textInfo.ToTitleCase(paciente);
+        }
+
+        #region Validación campos
+        /// <summary>
+        /// Evita que se active el errorProvider al hacer click en la X para cerrar el formulario
+        /// </summary>
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            e.Cancel = false;
+        }
+        private void cmbDoctor_Validating(object sender, CancelEventArgs e)
+        {
+            if (cmbDoctor.Text.Equals("Seleccione:"))
+            {
+                e.Cancel = true;
+                epDoctorValidar.SetError(cmbDoctor, "Campo requerido");
+            }
+            else
+            {
+                e.Cancel = false;
+                epDoctorValidar.SetError(cmbDoctor, "");
+            }
+        }
+        private void txtPaciente_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPaciente.Text))
+            {
+                e.Cancel = true;
+                epPacienteValidar.SetError(txtPaciente, "Campo requerido");
+            }
+            else
+            {
+                e.Cancel = false;
+                epPacienteValidar.SetError(txtPaciente, "");
+            }
+        }
+
+        private bool validarTipoMarcaColorAgregarProductos()
+        {
+            bool esValido = true;
+
+            if (cmbColor.Text.Equals("Seleccione:")) 
+            { 
+                epColorValidar.SetError(cmbColor, "Campo requerido");
+                esValido = false;
+            }else
+                epColorValidar.SetError(cmbColor, "");
+
+            if (cmbTipoProducto.Text.Equals("Seleccione:"))
+            {
+                epTipoProductoValidar.SetError(cmbTipoProducto, "Campo requerido");
+                esValido = false;
+            }else
+                epTipoProductoValidar.SetError(cmbTipoProducto, "");
+
+            if (cmbMarca.Text.Equals("Seleccione:"))
+            {
+                epMarcaValidar.SetError(cmbMarca, "Campo requerido");
+                esValido = false;
+            }
+            else
+                epMarcaValidar.SetError(cmbMarca, "");
+
+            return esValido; 
+        }
+        #endregion
+
+        private void cmbTipoProducto_Validating(object sender, CancelEventArgs e)
+        {
+            if (cmbTipoProducto.Text.Equals("Seleccione:"))
+            {
+                e.Cancel = true;
+                epTipoProductoValidar.SetError(cmbTipoProducto, "Campo requerido");
+            }
+            else
+            {
+                e.Cancel = false;
+                epTipoProductoValidar.SetError(cmbTipoProducto, "");
+            }
+        }
+
+        private void cmbMarca_Validating(object sender, CancelEventArgs e)
+        {
+            if (cmbMarca.Text.Equals("Seleccione:"))
+            {
+                e.Cancel = true;
+                epMarcaValidar.SetError(cmbMarca, "Campo requerido");
+            }
+            else
+            {
+                e.Cancel = false;
+                epMarcaValidar.SetError(cmbMarca, "");
+            }
+        }
+
+        private void cmbColor_Validating(object sender, CancelEventArgs e)
+        {
+            if (cmbColor.Text.Equals("Seleccione:"))
+            {
+                e.Cancel = true;
+                epColorValidar.SetError(cmbColor, "Campo requerido");
+            }
+            else
+            {
+                e.Cancel = false;
+                epColorValidar.SetError(cmbColor, "");
+            }
         }
     }
 }
