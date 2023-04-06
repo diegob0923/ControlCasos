bf39eed690364b663af15132acdaac6c5a0675bf
Diego <diegobarbozacoto@gmail.com>
2023-04-06 00:29:24 -0600

    save codigo que restaura la BD

--- ControlCasos/ControlCasos/Formularios/Seguridad/Respaldos.Designer.cs
+++ ControlCasos/ControlCasos/Formularios/Seguridad/Respaldos.Designer.cs
@@ -28,16 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(370, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // frmRespaldos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(860, 666);
+            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmRespaldos";
             this.Text = "Respaldos";
             this.ResumeLayout(false);
@@ -45,5 +57,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- ControlCasos/ControlCasos/Formularios/Seguridad/Respaldos.cs
+++ ControlCasos/ControlCasos/Formularios/Seguridad/Respaldos.cs
@@ -16,5 +16,13 @@ namespace ControlCasos.Formularios.Seguridad
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            
+
+        }
     }
 }
