c740bafc3ef22e1e2bbaf729b45f260bfb30c2a5
Diego <diegobarbozacoto@gmail.com>
2023-04-14 16:02:13 -0600

    restablecer contraseña en el centro

--- ControlCasos/ControlCasos/Formularios/RestablecerContrasena.Designer.cs
+++ ControlCasos/ControlCasos/Formularios/RestablecerContrasena.Designer.cs
@@ -187,6 +187,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmRestablecerContrasena";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Restablecer Contrasena";
             ((System.ComponentModel.ISupportInitialize)(this.epNuevaContrasenaValidar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.epConfirmarContrasenaValidar)).EndInit();
